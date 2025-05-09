afb9100f6b1b2bf57b607f4957186ea7382a0d77
aleks-2025 <vitaminviktor1@gmail.com>
2025-05-09 20:56:42 +0300

    new operators

---
+++ src/Variables/operators/Operators.java
@@ -0,0 +1,19 @@
+package Variables.operators;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 10, b = 3;
+        // оператор присваивания  =
+        //b = a;
+
+        // арифметические операторы
+
+        int sum = a + b;
+        int difference = b - a;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+    }
+}
