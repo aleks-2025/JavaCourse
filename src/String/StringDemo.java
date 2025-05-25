e5d2185dbbb7108c729423df7ac7d4e893806a0d
aleks-2025 <viktornautilus@gmail.com>
2025-05-25 08:27:46 +0300

    new

--- src/String/StringDemo.java
+++ src/String/StringDemo.java
@@ -17,19 +17,51 @@ public class StringDemo {
 
         // получение размера строки
 
-        System.out.println("The length of strLastname = "+strLastname.length());
+        System.out.println("The length of strLastname = " + strLastname.length());
 
         // получение подстроки
 
         String greeting = "Hello, World!";
-        String substring= greeting.substring(7);
+        String substring = greeting.substring(8);
         System.out.println(substring);
 
-        String substring2= greeting.substring(greeting.indexOf("o"),greeting.indexOf("d"));
+        String substring2 = greeting.substring(greeting.indexOf("o"), greeting.indexOf("d"));
         System.out.println(substring2);
 
-        String substring3= greeting.substring(greeting.indexOf("o"),11);
+        String substring3 = greeting.substring(greeting.indexOf("o"), 11);
         System.out.println(substring3);
+
+        // проверка наличия подстроки
+
+        System.out.println("Строка содержит \"World\" : " + greeting.contains("World"));
+        System.out.println("Строка содержит \"world\" : " + greeting.contains("world"));
+
+        String greetingWithUpperCase = greeting.toUpperCase();
+        String greetingWithLowerCase = greeting.toLowerCase();
+        System.out.println(greetingWithLowerCase);
+        System.out.println(greetingWithUpperCase);
+
+        System.out.println("Строка содержит \"WORLD\" : " + greetingWithLowerCase.contains("WORLD"));
+        System.out.println();
+
+        // удаление пробелов (trim)
+
+        var login = "    Nautilus-22    ";
+        System.out.println("login : " + login);
+        String loginAfterTrim = login.trim();
+        System.out.println("loginAfterTrim : " + loginAfterTrim);
+
+        // замена символа replace()
+
+        var loginAfterReplace = login.trim().replace("-", "__");
+        System.out.println("loginAfterReplace : " + loginAfterReplace);
+        System.out.println();
+        StringBuilder sb = new StringBuilder("   Hello, ");
+        sb.append("Viktor");
+        sb.append(" ");
+        sb.append("Nautilus!");
+        System.out.println(sb);
+
 
     }
 }
