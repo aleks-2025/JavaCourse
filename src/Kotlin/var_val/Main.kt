f4ff7190bfc3b09acead3131dedf380d928900e5
aleks-2025 <viktornautilus@gmail.com>
2025-05-25 08:53:12 +0300

    kotlin

---
+++ src/Kotlin/var_val/Main.kt
@@ -0,0 +1,16 @@
+package Kotlin.var_val
+
+fun main() {
+
+    // java
+    // int intVariable = 5"
+
+    // Kotlin
+    var intNumber = 5 // можно и так -  var intNumber:Int = 5
+    var someString = "My Name is Viktor" // var someStrng:String = "My Name is Viktor"
+
+    val nameOfManager = "Viktor Nautilus"
+    // поменять значение nameOfManager нельзя, это константа
+
+
+}
